3af1791c30220826dc48b8884e8e76ac032c6a6b
Anuj5588 <ak4826636@gmail.com>
2026-01-31 12:07:50 +0530

    fixed the mongofb error

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,34 @@
 import mongoose from "mongoose";
 
+const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://anuj5588:8v4TnH2t9ca1QXsN@cluster0.flx0wkc.mongodb.net/import_logs?appName=Cluster0";
 
+
+if (!MONGO_URI) {
+  throw new Error("MONGO_URI is not defined in environment variables");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI
+  if (cached.conn) {
+    return cached.conn;
+  }
 
- 
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGO_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => mongoose);
+  }
 
-  await mongoose.connect(`${uri}`);
+  cached.conn = await cached.promise;
   console.log("MongoDB connected");
+  return cached.conn;
 };
 
 export default connectDB;
